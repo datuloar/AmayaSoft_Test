794b92331e033be121792705df1d7bdc26a9aa4a
datuloar <58138724+datuloar@users.noreply.github.com>
2022-03-12 17:02:40 +0500

    Update PlayingField interface
    Remove usless modificator

--- Assets/GameName/Contracts/IPlayingField.cs
+++ Assets/GameName/Contracts/IPlayingField.cs
@@ -4,6 +4,6 @@ using System.Threading.Tasks;
 public interface IPlayingField
 {
     Task Insert(IEnumerable<ICard> card);
-    public void Create(LevelData levelData);
+    void Create(LevelData levelData);
     void Clear();
 }
